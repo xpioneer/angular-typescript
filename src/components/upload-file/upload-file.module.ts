67af47fb71527d5fed9b131bb91b577ef8f35bea
xpioneer <1286283280@qq.com>
2017-09-07 11:21:37 +0800

    add formmodule

--- src/components/upload-file/upload-file.module.ts
+++ src/components/upload-file/upload-file.module.ts
@@ -1,4 +1,5 @@
 import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { CommonModule }  from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { UploadFileComponent } from './upload-file.component'
@@ -6,6 +7,7 @@ import { UploadFileComponent } from './upload-file.component'
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule
     ],
     declarations: [
         UploadFileComponent,
