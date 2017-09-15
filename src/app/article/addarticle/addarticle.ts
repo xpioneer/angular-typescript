bc26aaccbeae7cdc2418eaab6c497d4ba61f0f09
xpioneer <1286283280@qq.com>
2017-09-15 22:45:05 +0800

    add quill editor

--- src/app/article/addarticle/addarticle.ts
+++ src/app/article/addarticle/addarticle.ts
@@ -1,35 +1,75 @@
 import { Router } from '@angular/router';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { NzNotificationService } from 'ng-zorro-antd';
 import { ArticleModel } from '../model/article.model';
 import { AddArticleTypeService } from './addarticle.service';
 
+const Quill = require('quill');
+
+
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': <any>[] }, { 'background': <any>[] }],          // dropdown with defaults from theme
+    [{ 'font': <any>[] }],
+    [{ 'align': <any>[] }],
+
+    ['link', 'image'],
+
+    ['clean']                                         // remove formatting button
+];
+
 @Component({
     selector: 'app-add-bank',
     templateUrl: './addarticle.html',
-    styles: []
+    styles: [`
+        .ql-container{min-height:600px;}
+    `]
 })
 export class AddArticleComponent implements OnInit {
-    formGroup: FormGroup;
     isConfirmLoading = false;
     addArticle:ArticleModel = new ArticleModel();
     @ViewChild('form') private form: NgForm;
+    @ViewChild('editor') private editor: ElementRef;
 
-    visible = {};
-    options: Array<any>;
-    cityLists: Array<any> = [];
-    cityChecked = {};
+    quillEditor = {};
 
     constructor(
         private router: Router,
-        private fb: FormBuilder,
         private addArticleTypeService: AddArticleTypeService,
         private notification: NzNotificationService
     ) { }
 
     ngOnInit() {
-      // this.getData();
+        console.log('ngOnInit', this.addArticle);
+        this.quillEditor = new Quill(this.editor.nativeElement, {
+            debug: false,
+            modules: {
+                toolbar: toolbarOptions
+            },
+            placeholder: '请写下你的想法...',
+            readOnly: false,
+            theme: 'snow'
+        })
+    }
+
+    ngAfterViewInit(){
+        console.log('ngAfterViewInit')
+    }
+
+    ngAfterContentInit(){
+        console.log('ngAfterContentInit')
     }
 
     save(){
--- src/app/article/model/article.model.ts
+++ src/app/article/model/article.model.ts
@@ -3,11 +3,11 @@
 export class ArticleModel {
     public id: string;
     public title: string;
-    public is_top: number;
+    public is_top: number = 0;
     public abstract: string;
     public pics: string;
     public praise: number;
     public view_count: number;
-    public is_original: boolean;
+    public is_original: boolean = true;
     public created_at: string;
 }
