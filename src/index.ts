a69b25a863ed15724110c03959214815e4433589
xpioneer <1286283280@qq.com>
2017-11-27 16:49:39 +0800

    fix css

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import './assets/less/index.less';
-import 'quill/dist/quill.bubble.css';
-import 'quill/dist/quill.core.css';
+// import 'quill/dist/quill.bubble.css';
+// import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
 
 declare var module: any;
